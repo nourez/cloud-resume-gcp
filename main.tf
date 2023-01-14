8142d77294b24090d7353811a31ff3f4f7fa157c
Nourez Rawji <nourez@nourez.dev>
2023-01-13 23:45:42 -0500

    Created simple pipelines for Dev
    * updated name of placeholder action
    
    * Changed some wording
    
    * switched to branches-ignore
    
    * Added basic Terraform config
    
    * added a test bucket
    
    * fixed typo in bucket name
    
    * created a destroy script

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  cloud {
+    organization = "nourezrawji"
+
+    workspaces {
+      name = "cloud-resume-dev"
+    }
+  }
+}
+
+provider "google" {
+  project = "cloud-resume-dev"
+  region  = "northamerica-northeast2"
+  zone    = "northamerica-northeast2-b"
+}
+
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
+# Create a bucket to store the static website 
+resource "google_storage_bucket" "static_website" {
+  name                        = "terraform-test-${random_id.instance_id.hex}"
+  location                    = "NORTHAMERICA-NORTHEAST2"
+  storage_class               = "STANDARD"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "error.html"
+  }
+}
+
+resource "google_storage_bucket" "test_bucket" {
+  name          = "another-test-${random_id.instance_id.hex}"
+  location      = "NORTHAMERICA-NORTHEAST1"
+  force_destroy = true
+}
