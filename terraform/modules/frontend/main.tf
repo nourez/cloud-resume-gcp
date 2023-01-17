c1d31ef1bec8db3c1da623a723218ea164d25593
Nourez Rawji <nourez@nourez.dev>
2023-01-16 22:40:15 -0500

    24 create a static bucket for the website files (#27)
    * created publicly bucket for static website content
    
    * added public access policy for uniform bucket=

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -2,10 +2,19 @@ resource "random_id" "instance_id" {
   byte_length = 8
 }
 
+resource "google_storage_bucket_iam_binding" "public_access_policy" {
+  bucket = google_storage_bucket.static_website.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "allUsers",
+  ]
+}
+
 # Create a bucket to store the static website 
 resource "google_storage_bucket" "static_website" {
-  name                        = "terraform-test-${random_id.instance_id.hex}"
-  location                    = "NORTHAMERICA-NORTHEAST2"
+  name                        = "cloud-resume-${random_id.instance_id.hex}"
+  location                    = "US"
   storage_class               = "STANDARD"
   force_destroy               = true
   uniform_bucket_level_access = true
@@ -16,8 +25,3 @@ resource "google_storage_bucket" "static_website" {
   }
 }
 
-resource "google_storage_bucket" "test_bucket" {
-  name          = "another-test-${random_id.instance_id.hex}"
-  location      = "NORTHAMERICA-NORTHEAST1"
-  force_destroy = true
-}
