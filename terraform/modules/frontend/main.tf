ab75d6704d27b2255d5f87322b5014459b21177b
Nourez Rawji <nourez@nourez.dev>
2023-01-22 13:37:45 -0500

    create static content deployments for website
    * Added static resources to bucket
    
    * added github oidc for gcp
    
    * added oidc for dev-destroy
    
    * configured oidc for uat pipelines
    
    * added oidc for prod pipeline

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -25,3 +25,37 @@ resource "google_storage_bucket" "static_website" {
   }
 }
 
+resource "google_storage_bucket_object" "index_html" {
+  name         = "index.html"
+  bucket       = google_storage_bucket.static_website.name
+  source       = "../../frontend/index.html"
+  content_type = "text/html"
+}
+
+resource "google_storage_bucket_object" "error_html" {
+  name         = "error.html"
+  bucket       = google_storage_bucket.static_website.name
+  source       = "../../frontend/error.html"
+  content_type = "text/html"
+}
+
+resource "google_storage_bucket_object" "styles_css" {
+  name         = "styles.css"
+  bucket       = google_storage_bucket.static_website.name
+  source       = "../../frontend/styles.css"
+  content_type = "text/css"
+}
+
+resource "google_storage_bucket_object" "index_js" {
+  name         = "index.js"
+  bucket       = google_storage_bucket.static_website.name
+  source       = "../../frontend/index.js"
+  content_type = "text/javascript"
+}
+
+resource "google_storage_bucket_object" "nourez_jpg" {
+  name         = "nourez.jpg"
+  bucket       = google_storage_bucket.static_website.name
+  source       = "../../frontend/nourez.jpg"
+  content_type = "image/jpeg"
+}
