5150121c81c8f7cdd0d207788f716bdd383c043d
nourez <nourez@nourez.dev>
2023-01-23 20:46:39 -0500

    Create a frontend for the load balancer

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -88,11 +88,19 @@ resource "google_compute_url_map" "resume_url_map" {
   default_service = google_compute_backend_bucket.resume_backend.self_link
 }
 
-# Create a target proxty to route requests to the backend bucket
+# Create a load balancer to serve the static website
 resource "google_compute_target_https_proxy" "resume_target_proxy" {
   name    = "cloud-resume-target-proxy"
   url_map = google_compute_url_map.resume_url_map.self_link
   ssl_certificates = [
     google_compute_managed_ssl_certificate.resume_cert.self_link,
   ]
+}
+
+# Add a frontend to the load balancer
+resource "google_compute_global_forwarding_rule" "resume_forwarding_rule" {
+  name       = "cloud-resume-forwarding-rule"
+  target     = google_compute_target_https_proxy.resume_target_proxy.self_link
+  ip_address = google_compute_global_address.resume_ip.address
+  port_range = "443"
 }
