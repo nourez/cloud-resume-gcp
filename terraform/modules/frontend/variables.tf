ee2b4286bd6eac3e21976f8dcff7dba82d08f16c
nourez <nourez@nourez.dev>
2023-01-24 22:24:37 -0500

    add www cert

--- terraform/modules/frontend/variables.tf
+++ terraform/modules/frontend/variables.tf
@@ -1,5 +1,5 @@
 variable "cert_domains" {
   description = "Domains for the certificate"
   type        = list(string)
-  default     = ["nourez.dev"]
+  default     = ["nourez.dev", "www.nourez.dev"]
 }
