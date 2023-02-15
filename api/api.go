904d7c353377ab6be56cf6630e16e6c08dcbf07c
nourez <nourez@nourez.dev>
2023-02-15 18:59:36 -0500

    cleaned up formatting

--- api/api.go
+++ api/api.go
@@ -8,85 +8,88 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
+const (
+	defaultPageURL         = "rootPage"
+	defaultGoogleProjectID = "cloud-resume-sandbox"
+)
+
 func createClient(ctx context.Context) *firestore.Client {
-	client, err := firestore.NewClient(ctx, "cloud-resume-sandbox")
+	client, err := firestore.NewClient(ctx, defaultGoogleProjectID)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 	return client
 }
 
-func pageExists(pageUrl string) bool {
-	ctx := context.Background()
-	client := createClient(ctx)
-	defer client.Close()
-
+func pageExists(ctx context.Context, client *firestore.Client, pageURL string) bool {
 	// Check if document with pageUrl exists
-	_, err := client.Collection("hitCounts").Doc(pageUrl).Get(ctx)
-	if err != nil {
-		return false
-	}
-
-	return true
+	_, err := client.Collection("hitCounts").Doc(pageURL).Get(ctx)
+	return err == nil
 }
 
 func incrementCounterEndpoint(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	const (
+		methodNotAllowed    = http.StatusMethodNotAllowed
+		internalServerError = http.StatusInternalServerError
+		ok                  = http.StatusOK
+	)
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", methodNotAllowed)
 		return
 	}
 
-	pageUrl := r.FormValue("pageUrl")
-	if pageUrl == "" {
-		pageUrl = "rootPage"
+	pageURL := r.FormValue("pageUrl")
+	if pageURL == "" {
+		pageURL = defaultPageURL
 	}
 
 	// Open connection to Firestore
 	ctx := context.Background()
-	firestoreConnection := createClient(ctx)
-	defer firestoreConnection.Close()
+	client := createClient(ctx)
+	defer client.Close()
 
-	pageDoc := firestoreConnection.Collection("hitCounts").Doc(pageUrl)
+	pageDoc := client.Collection("hitCounts").Doc(pageURL)
 
-	// Check if document with pageUrl exists
+	// Check if document with pageURL exists
 	// If it does not exist, create it
-	if !pageExists(pageUrl) {
+	if !pageExists(ctx, client, pageURL) {
 		_, err := pageDoc.Set(ctx, map[string]interface{}{
 			"hitCount": 0,
-			"pageUrl":  pageUrl,
+			"pageUrl":  pageURL,
 		})
 		if err != nil {
-			http.Error(w, "Failed to create document", http.StatusInternalServerError)
+			http.Error(w, "Failed to create document", internalServerError)
 			return
 		}
-
-		log.Printf("Created document for pageUrl: %s", pageUrl)
+		log.Printf("Created document for pageURL: %s", pageURL)
 	}
 
 	// Get hitCount
 	doc, err := pageDoc.Get(ctx)
 	if err != nil {
-		http.Error(w, "Failed to get hitCount", http.StatusInternalServerError)
+		http.Error(w, "Failed to get hitCount", internalServerError)
 		return
 	}
 
 	hitCount, err := doc.DataAt("hitCount")
 	if err != nil {
-		http.Error(w, "Failed to get hitCount", http.StatusInternalServerError)
+		http.Error(w, "Failed to get hitCount", internalServerError)
 		return
 	}
 
 	// Increment hitCount
 	_, err = pageDoc.Update(ctx, []firestore.Update{
-		{Path: "hitCount", Value: hitCount.(int64) + 1},
+		{Path: "hitCount", Value: int(hitCount.(int64)) + 1},
 	})
 	if err != nil {
-		http.Error(w, "Failed to increment hitCount", http.StatusInternalServerError)
+		http.Error(w, "Failed to increment hitCount", internalServerError)
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Successfully incremented " + pageUrl))
+	w.WriteHeader(ok)
+	w.Write([]byte("{\"status\": \"ok\"}"))
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func main() {
